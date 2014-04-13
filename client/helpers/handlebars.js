c16aff9d2ee681b8aca433cf215bd94f76597d1e
markgaucher <markgaucher@gmail.com>
2014-04-13 15:32:31 -0400

    updated map colors

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -2,11 +2,28 @@ Handlebars.registerHelper('average', function(total, count) {
   return (total / count).toFixed(2);
 })
 
-Handlebars.registerHelper('positive', function(mood) {
-  if (mood > 0) {
-    return true;
+Handlebars.registerHelper('sentiment', function(total, count) {
+  var mood = (total / count);
+  if (mood > 1.0) {
+    return 'happier';
+  } else if (mood > 0.5) {
+    return 'happy';
+  } else if (mood > 0) {
+    return 'neutral';
+  } else if (mood > -1.0) {
+    return 'sad';
   } else {
-    return false;
+    return 'sadder';
+  }
+})
+
+Handlebars.registerHelper('weight', function(count) {
+  if (count > 5000) {
+    return 'strong';
+  } else if (count > 500) {
+    return 'normal';
+  } else {
+    return 'light';
   }
 })
 

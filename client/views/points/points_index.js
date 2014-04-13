23470c958b8b0499a8bdaaedaa50e43a5c99365b
markgaucher <markgaucher@gmail.com>
2014-04-13 18:56:58 -0400

    delay load until client database is loaded

--- client/views/points/points_index.js
+++ client/views/points/points_index.js
@@ -1,5 +1,17 @@
+Deps.autorun(function() {
+  Meteor.subscribe("points", {
+    onReady: function() {
+      Session.set("active", true);
+    }
+  })
+});
+
 Template.pointsIndex.helpers({
   points: function() {
-    return Points.find({'latitude': {$gt: 21, $lt: 55}, 'longitude': {$gt: -132, $lt: -58}}, {sort: {'latitude': 1, 'longitude': 1}});
+    if(Session.get("active")) {
+      return Points.find({'latitude': {$gt: 21, $lt: 55}, 'longitude': {$gt: -132, $lt: -58}}, {sort: {'latitude': 1, 'longitude': 1}});
+    } else {
+      return [];
+    }
   }
-})
+});

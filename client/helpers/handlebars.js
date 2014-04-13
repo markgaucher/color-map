2950a008dfc482e6ff304cf5ca417eb455a14ebc
markgaucher <markgaucher@gmail.com>
2014-04-12 20:34:18 -0400

    color code number results

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -1,3 +1,11 @@
 Handlebars.registerHelper('average', function(total, count) {
   return (total / count).toFixed(2);
 })
+
+Handlebars.registerHelper('positive', function(mood) {
+  if (mood > 0) {
+    return true;
+  } else {
+    return false;
+  }
+})
